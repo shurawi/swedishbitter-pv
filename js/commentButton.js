e9773ee852c2fbcc24509b16b3c8a935702f454e
shurawi <denis.b.toropov@gmail.com>
2021-03-28 22:26:17 +0700

    adds files

---
+++ js/commentButton.js
@@ -0,0 +1,34 @@
+//В блоке 'comment' при нажатии на 'comment__button' переключает русскоязычные отзывы на англоязычные и на оборот.
+
+let commentContainerRu = document.querySelector('.comment__container--ru');
+let commentContainerEn = document.querySelector('.comment__container--en');
+let commentButtonRu = document.querySelector('.comment__button--ru');
+let commentButtonEn = document.querySelector('.comment__button--en');
+let bannerButtom = document.querySelector('.banner__buttom');
+let storiesBtn = document.querySelector('.stories__btn');
+
+commentButtonRu.addEventListener ('click', function() {
+  commentButtonRu.classList.remove('comment__button--disable');
+  commentButtonRu.classList.add('comment__button--enable');
+  commentButtonEn.classList.remove('comment__button--enable');
+  commentButtonEn.classList.add('comment__button--disable');
+  commentContainerRu.classList.remove('comment__container--disable')
+  commentContainerEn.classList.add('comment__container--disable')
+})
+
+commentButtonEn.addEventListener ('click', function() {
+  commentButtonEn.classList.remove('comment__button--disable');
+  commentButtonEn.classList.add('comment__button--enable');
+  commentButtonRu.classList.add('comment__button--disable');
+  commentButtonRu.classList.remove('comment__button--enable');
+  commentContainerEn.classList.remove('comment__container--disable')
+  commentContainerRu.classList.add('comment__container--disable')
+})
+
+bannerButtom.addEventListener('click', function (evt) {
+  evt.preventDefault();
+})
+
+storiesBtn.addEventListener('click', function (evt) {
+  evt.preventDefault();
+})
